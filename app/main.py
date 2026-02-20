553edcead68a401da9496e26bd98d2910fa783d9
Sergio-zt <fop.gso@gmail.com>
2026-02-19 19:44:47 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -8,11 +8,21 @@ def cinema_visit(
     movie: str,
     customers: list,
     hall_number: int,
-    cleaner: str,    
-) -> None:    
-    result_customers = [Customer(name=customer["name"], food=customer["food"]) for customer in customers]
-    for c in result_customers:
-        CinemaBar.sell_product(product=c.food, customer=c)
+    cleaner: str
+) -> None:
+    if not isinstance(customers, list):
+        (hall_number, cleaner, movie, customers) = (
+            customers, hall_number, cleaner, movie
+        )
+    result_customers = [
+        Customer(name=customer["name"], food=customer["food"])
+        for customer in customers
+    ]
+    for result_customer in result_customers:
+        CinemaBar.sell_product(
+            product=result_customer.food,
+            customer=result_customer
+        )
     cleaning_staff = Cleaner(name=cleaner)
     hall = CinemaHall(hall_number)
     hall.movie_session(
